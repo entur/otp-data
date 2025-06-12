d25426b8f569c1ad103d5954ea440b625b67b03d
Thomas Gran <t2gran@gmail.com>
2025-06-12 14:05:18 +0200

    refactor: Move classes to own files

--- src/main/config/model/SetupCase.kt
+++ src/main/config/model/SetupCase.kt
@@ -1,17 +1,6 @@
 package config.model
 
-import java.io.File
-
-data class WebResource(val name : String, val url: String, val file: String) {
-  fun fullUrl() : String = "$url/$file"
-
-  fun asFile(dir : File) = File(dir, file)
-  override fun toString(): String {
-    return "$name $url/$file"
-  }
-}
-
-data class Case(
+data class SetupCase(
     val name: String,
     val osm: List<WebResource>,
     val geojson: List<String>,
